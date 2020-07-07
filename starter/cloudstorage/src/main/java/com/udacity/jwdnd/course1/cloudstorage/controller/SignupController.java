e262097036aca52935742953250d7f1108fa5b8e
xlsarath <xlsarathchandra@gmail.com>
2020-07-07 11:52:15 -0700

    added getmapping changes to signup

--- starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignupController.java
@@ -4,6 +4,7 @@ import com.udacity.jwdnd.course1.cloudstorage.model.User;
 import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ public class SignupController {
         this.userService = userService;
     }
 
+    @GetMapping()
     public String signupView(){
         return "signup";
     }
